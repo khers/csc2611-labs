bb7db41a6d9e21a73d28ac1f29afd5c9b67b1776
Eric B Munson <ebm@cs.toronto.edu>
2020-02-03 14:48:27 -0500

    Finish part 2 of lab 1
    Signed-off-by: Eric B Munson <ebm@cs.toronto.edu>

---
+++ lab1_2.py
@@ -0,0 +1,147 @@
+#!/usr/bin/python3
+
+import numpy as np
+import pickle
+import pandas as pd
+from scipy.linalg import orthogonal_procrustes
+from sklearn.metrics.pairwise import cosine_similarity
+from gensim.models.keyedvectors import WordEmbeddingsKeyedVectors
+from scipy.stats import pearsonr
+
+
+words = []
+
+
+def read_and_organize_models(path):
+    global words
+    stored = {}
+    with open(path, 'rb') as f:
+        stored = pickle.load(f)
+
+    decade_index = 0
+    models_by_decade = {}
+    for decade in stored['d']:
+        all_rows = []
+        word_index = 0
+        for word in stored['w']:
+            all_rows.append(stored['E'][word_index][decade_index])
+            word_index += 1
+        models_by_decade[decade] = pd.DataFrame(all_rows, index=stored['w'])
+        decade_index += 1
+    words = stored['w']
+    return models_by_decade
+
+
+def compute_rotated_similarity(early, later):
+    R,_ = orthogonal_procrustes(early, later, check_finite=False)
+    rotated = early @ R
+    sims = cosine_similarity(rotated.to_numpy(), later.to_numpy())
+    ret = list()
+    for i in range(0, len(sims)):
+        ret.append(sims[i][i])
+    return ret
+
+
+def get_similar_set(word, model):
+    similar = set()
+    values = model.most_similar(word, topn=20)
+    for v in values:
+        similar.add(v[0])
+    return similar
+
+
+def compute_similar_nn(early, later):
+    M_early = WordEmbeddingsKeyedVectors(300)
+    M_early.add(words, early.to_numpy())
+    M_later = WordEmbeddingsKeyedVectors(300)
+    M_later.add(words, later.to_numpy())
+
+    scores = list()
+    for word in words:
+        early_similar = get_similar_set(word, M_early)
+        later_similar = get_similar_set(word, M_later)
+        count = len(early_similar.intersection(later_similar))
+        scores.append(count)
+    return scores
+
+
+def compute_scaled_average_sim(early, later):
+    M_early = WordEmbeddingsKeyedVectors(300)
+    M_early.add(words, early.to_numpy())
+    M_later = WordEmbeddingsKeyedVectors(300)
+    M_later.add(words, later.to_numpy())
+
+    scores = list()
+    for word in words:
+        score = 0
+        early_values = M_early.most_similar(word, topn=20)
+        later_values = M_later.most_similar(word, topn=20)
+        early_dict = {v[0]:v[1] for v in early_values}
+        later_dict = {v[0]:v[1] for v in later_values}
+        overlap = set([w for w in early_dict.keys() if w in later_dict])
+        early_avg = 0
+        later_avg = 0
+        for entry in overlap:
+            early_avg += early_dict[entry]
+            later_avg += later_dict[entry]
+        early_avg = early_avg / len(overlap) if len(overlap) else 0
+        later_avg = later_avg / len(overlap) if len(overlap) else 0
+        scores.append(len(overlap) + (1 - abs(later_avg - early_avg)))
+    return scores
+
+if __name__ == "__main__":
+    models_by_decade = read_and_organize_models('./data.pkl')
+    cosine_sim_scores = compute_rotated_similarity(models_by_decade[1900], models_by_decade[1990])
+    nn_count_scores = compute_similar_nn(models_by_decade[1900], models_by_decade[1990])
+    scaled_avg_scores = compute_scaled_average_sim(models_by_decade[1900], models_by_decade[1990])
+
+    print("Pearson correlation cosine to NN overlap count {}".format(pearsonr(cosine_sim_scores, nn_count_scores)[0]))
+    print("Pearson correlation cosine to scaled overlap {}".format(pearsonr(cosine_sim_scores, scaled_avg_scores)[0]))
+    print("Pearson correlation NN overlap count to scaled overlap {}".format(pearsonr(nn_count_scores, scaled_avg_scores)[0]))
+
+    cos = list(zip(words, cosine_sim_scores))
+    nn = list(zip(words, nn_count_scores))
+    scale = list(zip(words, scaled_avg_scores))
+
+    cos.sort(key=lambda v: v[1], reverse=True)
+    nn.sort(key=lambda v: v[1], reverse=True)
+    scale.sort(key=lambda v: v[1], reverse=True)
+    print("rotated cosine twenty least changed {}".format([w[0] for w in cos[:20]]))
+    print("rotated cosine twenty most changed {}\n".format([w[0] for w in cos[-20:]]))
+
+    print("nearest neighbor set twenty least changed {}".format([w[0] for w in nn[:20]]))
+    print("nearest neighbor set most changed {}\n".format([w[0] for w in nn[-20:]]))
+
+    print("nearest neighbor set + avg distance twenty least changed {}".format([w[0] for w in scale[:20]]))
+    print("nearest neighbor set + avg distance most changed {}".format([w[0] for w in scale[-20:]]))
+
+    c_d = dict(cos)
+    n_d = dict(nn)
+    s_d = dict(scale)
+
+    raw_truth = ["america 1", "finger 6", "file 8", "towns 2", "purchase 3", "particles 0", "earth 4",
+            "prisoner 1", "goal 2", "artist 2", "sciences 4", "bill 5", "corn 2", "anyone 0",
+            "foundation 3", "opportunity 4", "tongue 5", "haven 1", "meal 5", "regret 0", "appointment 1",
+            "zealand 0", "efforts 1", "code 10", "attack 7", "urine 0", "test 7", "surprise 4", "document 1",
+            "type 6", "windows 9", "habit 4", "breast 2", "harvard 0", "assistance 2", "end 3", "use 6",
+            "pacific 2", "respects 3", "procedure 2", "printing 6", "character 4", "smoke 5", "remainder 1",
+            "world 4", "manners 3", "clergy 0", "quarters 5", "faith 4", "speaker 6"]
+    truth_words = list()
+    truth_scores = list()
+    for item in raw_truth:
+        word,score = item.split()
+        truth_words.append(word)
+        truth_scores.append(int(score))
+
+    c_scores = list()
+    n_scores = list()
+    s_scores = list()
+
+    for word in truth_words:
+        c_scores.append(c_d[word])
+        n_scores.append(n_d[word])
+        s_scores.append(s_d[word])
+
+    print("Pearson with cos {}".format(pearsonr(c_scores, truth_scores)[0]))
+    print("Pearson with NN {}".format(pearsonr(n_scores, truth_scores)[0]))
+    print("Pearson with NN + avg {}".format(pearsonr(s_scores, truth_scores)[0]))
